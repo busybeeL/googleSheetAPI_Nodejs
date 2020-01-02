6df6c52821e379e5a4872a9cf566f1ea96c77d13
jiwoo <jiwoo.kim@codeworldwide.com>
2020-01-02 16:28:01 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var GoogleSheets = require('google-drive-sheets')
+var express = require('express')
+var app = express()
+var path = require('path')
+var PORT = process.env.PORT || 8000
+
+var mySheet = new GoogleSheets('1kib56LbVv1cWgGDfc2fyTQMzYtVVIT54dL36G1PbSh8')
+app.set('view engine', 'pug')
+
+mySheet.getRows(1, function(err, rowData){
+    console.log(rowData)
+    var titles = []
+    var contents = []
+    for (var i = 0; i < rowData.length; i++){
+        var itemT = rowData[i].name
+        var itemC = rowData[i].grade
+        titles += ' ' + itemT
+        contents += ' ' + itemC
+    }
+    var titleS = titles.split(' ')
+    var titleL = titleS.filter(Boolean)
+    var contentS = contents.split(' ')
+    var contentL = contentS.filter(Boolean)
+
+    console.log(titleL)
+    console.log(contentL)
+
+    app.get('/', function(req, res){
+        res.render(
+            'index',
+            { title: titleL, content: contentL }
+        )
+    })
+
+    app.listen(PORT, function(){
+        console.log('Example app listening on port', PORT)
+    })
+})
